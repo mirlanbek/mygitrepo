8a1e1c43d603b0ba4fca159e676f2ed5ccaa404d
mirlanbek <mirlan_t@rambler.ru>
2015-05-24 01:05:30 -0500

    I'm adding the script how to block the ip tried 3 times to access

---
+++ fail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ount=$(cat /var/log/secure | grep -w Failed | awk '{print $11}'| sort | uniq -c | awk '{print $1}')
+
+if [ $count -eq 3  ]
+then
+ip=$(cat /var/log/secure | grep -w Failed | awk '{print $11}'| sort | uniq)
+iptables -A INPUT -s $ip -j DROP
+fi
+
