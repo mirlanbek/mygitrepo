3d9b43a9057d7411f15a337719ff5ede9481b857
mirlanbek <mirlan_t@rambler.ru>
2015-05-24 01:24:07 -0500

    added m

--- fail.sh
+++ fail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ount=$(cat /var/log/secure | grep -w Failed | awk '{print $11}'| sort | uniq -c | awk '{print $1}')
+count=$(cat /var/log/secure | grep -w Failed | awk '{print $11}'| sort | uniq -c | awk '{print $1}')
 
 if [ $count -eq 3  ]
 then
