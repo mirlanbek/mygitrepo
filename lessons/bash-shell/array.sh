5e31c1b38f1de72e1f99fe50714016cd0ae4c674
root <root@cpt-bmp022.jf.intel.com>
2018-03-05 11:03:43 -0800

    new bash syntax in array.sh

--- lessons/bash-shell/array.sh
+++ lessons/bash-shell/array.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
+
+
+# ******************* new syntax arrays *************************
+
 array=("bir" "eki" "uch" 7)
 
 #if [[ " ${array[@]} " =~ " ${value} " ]]; then
 if [[ " ${array[@]} " =~ "uch" ]]; then
 echo bar  
-array+=(8)
+
+array+=(8)   # append into array
 
 echo ${array[@]}
 
-aa=` expr ${#array[@]} - 1` 
+
+aa=` expr ${#array[@]} - 1`   # count array length and -1 
 echo ${array[$aa]} 
    # whatever you want to do when arr contains value
 fi
 
+# ******************* new syntax for if *************************
+
 if grep -q rr test.sh ;then
 echo bar eken
 fi
+
+# **************** change value of GRUB_DEFAULT **************************
+
+sed  "s/^\(GRUB_DEFAULT=\).*/\1Mirlan/g" grub
+
+sed 's,^\(GRUB_DEFAULT=\).*$,\1mirlan,g' grub   # use ","  instead of  "/"
+
+
 
