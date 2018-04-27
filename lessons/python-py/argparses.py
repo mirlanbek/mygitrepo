8f4c3c86e676b7bb2ff13ca9d03bc1e7bd1cf170
lab <lab@cpt-bmp003.jf.intel.com>
2018-04-27 09:10:42 -0700

    I'm adding args code in argparses.py

--- lessons/python-py/argparses.py
+++ lessons/python-py/argparses.py
@@ -52,3 +52,79 @@ def family (miki,nurken):
 family(args.miki, args.nurken)
 ################ on comand line############   python start.py -m "Mirlanbek" -n "Nurkenbek"   ###############
 
+#!/usr/bin/python
+import os, sys
+import argparse
+
+
+
+class family (object):
+    lastname = "Tokonbekov"
+
+    def __init__(self,a_jurt,t_jurt,k_jurt):
+        self.a_jurt = a_jurt
+        self.k_jurt = k_jurt
+        self.t_jurt = t_jurt
+
+
+    @property
+    def member(self):
+        def is_beka():
+            if self.a_jurt == "savai" and self.t_jurt == "monok":
+                return True
+            else:
+                return False
+            return True
+
+        def is_emil():
+            if self.a_jurt == "savai" and self.t_jurt == "latysh":
+                return True
+            else:
+                return False
+            return True
+
+
+
+        if is_beka():
+            return "Menin atym Beka Men Mirlandyn balasymyn i have no " +self.k_jurt
+        if is_emil():
+            return "Menin atym Emil Men Nurkendin balasy and i have no " + self.k_jurt
+
+parser = argparse.ArgumentParser(description='Family Info')
+
+parser.add_argument("-a", "--a_jurt", default = False, help = "ata jurtunu aitasin")
+parser.add_argument("-t", "--t_jurt", default = False, help = "taga jurtunu aitasin")
+parser.add_argument("-k", "--k_jurt", default = False, help = "kainy jurtunu aitasin")
+args = parser.parse_args()
+argsl = vars(args)
+
+def check_args(argumentss):
+    for i in argsl.keys():
+        if argsl[i]:
+            return True
+        else:
+            return False
+    return True
+
+if check_args(argsl):
+
+
+    a_jurt = args.a_jurt
+    k_jurt = args.k_jurt
+    t_jurt = args.t_jurt
+
+
+else:
+
+    a_jurt = "savai"
+    k_jurt = "Kayny jurt"
+    t_jurt = "latysh"
+
+
+
+p1=family(a_jurt,t_jurt,k_jurt)
+
+print(p1.member)
+
+
+################ on comand line############   python start.py -a "savai" -t "monok" -k kayny   ###############
