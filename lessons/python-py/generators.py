defefc12e299f5eac7e39a47c4c388b225894403
mirlan <miki@miki.com>
2019-09-16 11:26:45 -0700

    update generators

--- lessons/python-py/generators.py
+++ lessons/python-py/generators.py
@@ -1,3 +1,8 @@
+
+# Generator is "yield", and it is used like "return" in a function only, but returns when you call with "next()"
+# for i in 1000000000000: here generator helps loop by one and stop, otherwise memory cannot handle big number  
+
+
 # my_nums = iter([1,2,3,4])
 
 # for i in my_nums:
@@ -57,3 +62,27 @@ print(next(p1))
 # for t in my_nums:
 #     print(type(my_nums))
 #     print(t)    
+
+
+def gerator(list):
+    for i in list:
+        yield i
+
+list = ["bir", "eki", "uch", "tort", "besh"]
+
+a = gerator(list)
+
+o = len(list)
+while  o > 0:
+    print(  next(a)  )
+    o -= 1
+
+# +++++++++++++++++++++++++++++++++++++
+
+def gener(st):
+    yield st
+
+a = gener("Miki")
+
+print (next(a))
+
