085da361c9768135043db659fe6d97be470a876f
mirlan <miki@miki.com>
2019-09-25 21:53:17 -0700

    add __getitem__.py

---
+++ lessons/python-py/__getidem__.py
@@ -0,0 +1,42 @@
+from string import ascii_letters
+
+class MyContainer(object):
+
+    def __getitem__(self, key):
+        return ascii_letters[key]           #  __getitem__ we need it when we pass indexing itself like here 
+
+
+my_container = MyContainer()  
+
+print(ascii_letters[5])
+print(my_container[0])  # a
+print(my_container[16])  # q
+
+
+
+#------------------------------------------------------
+
+class CustomList(object):
+    def __init__(self, elements=0):
+        self.my_custom_list = [0] * elements
+
+    def __setitem__(self, index, value):
+        self.my_custom_list[index] = value
+
+    def __getitem__(self, index):
+        return "Hey you are accessing {} element whose value is: {}".format(index, self.my_custom_list[index])
+
+    def __str__(self):
+        return str(self.my_custom_list)
+
+obj = CustomList(12)
+obj[0] = 1
+print(obj[0])
+print(obj)
+
+
+# ----------------------------------------------------
+
+
+
+
