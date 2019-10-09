60c04fe574ad44047080815f9f703106f25c2596
mirlan <miki@miki.com>
2019-10-08 20:55:01 -0700

    updated ansible module

--- lessons/ansible/ansible_module.py
+++ lessons/ansible/ansible_module.py
@@ -45,16 +45,20 @@ _____________________
 
   tasks:
   - name: Test that my change_version module works
-    version_change:
+    version_change: 
       version_name: "Before"
-      version_no:  1.1.1
+      version_no:  1.1.1 
       unchanged_value: "This will pass through"
     register: result
 
-  - debug: var=result
+  - debug: var=result                                # Note:  result contains vars from version_change: version_name, version_no etc,.
+  - debug: var=test
+  - debug: var=numa_nodes                            # these vars are facts, added by library "ansible_facts.update"
+
 
 #---------------------------------------------------------------------
 vi library/version_change.py
+____________________________________
 
 #!/usr/bin/python
 
@@ -66,10 +70,18 @@ def main():
         "version_no": {"default": True, "type": "str"},
         "version_name": {"default": True, "type": "str"},
         "unchanged_value": {"default": True, "type": "str"}
+
     }
 
     module = AnsibleModule(argument_spec=fields)
+    params = module.params
     # change the name
+    ansible_facts = {"test": "Test_value"}
+    ansible_facts.update({
+        "interface_numa_cpu_lists": "cpu_lists2",
+        "numa_nodes": "numa_nodes2",
+        "other_numa_node_cores": "sorted(other_numa_node_cores)"
+    })
     module.params.update({"version_name": "After"})
     # bump minor and patch version
     mylist = module.params["version_no"].split('.')
@@ -78,8 +90,8 @@ def main():
     mystr= '.'.join(mylist)
     module.params.update({"version_no": mystr})
 
-    
-    module.exit_json(changed=True, meta=module.params)
+
+    module.exit_json(changed=True, ansible_facts=ansible_facts)
 
 
 if __name__ == '__main__':
