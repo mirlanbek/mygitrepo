343f502ccf1a567ccc39667daf0cfa69e8dfc95f
mirlan <miki@miki.com>
2019-11-11 22:44:06 -0800

    dict methods()

--- lessons/python-py/dic.py
+++ lessons/python-py/dic.py
@@ -94,3 +94,44 @@ print(mydict)
 
 
 
+# ************************* 888888888888888888888888
+
+clear()	Removes all the elements from the dictionary
+copy()	Returns a copy of the dictionary
+fromkeys()	Returns a dictionary with the specified keys and values
+get()	Returns the value of the specified key
+items()	Returns a list containing a tuple for each key value pair
+keys()	Returns a list containing the dictionary keys
+pop()	Removes the element with the specified key
+popitem()	Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	Updates the dictionary with the specified key-value pairs
+values()	Returns a list of all the values in the dictionary
+
+# ************************* 888888888888888888888888
+
+import collections
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+mashina = {"type": "SUB"}
+
+car.update({"color": "White"})
+print(car)
+car.update(mashina)
+
+a="Name"
+b="Mirlan"
+
+coll = collections.OrderedDict()
+coll[a] = b
+car.update(coll)
+print(car)
+
+# ************************* 888888888888888888888888
+
+
