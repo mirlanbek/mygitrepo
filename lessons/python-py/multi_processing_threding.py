496e1f3cf2c132e7bcc1ba0925652d73c9588851
mirlan <mirlan@mirlan.com>
2019-11-24 20:24:58 -0800

    process concurrency is aded

--- lessons/python-py/multi_processing_threding.py
+++ lessons/python-py/multi_processing_threding.py
@@ -171,3 +171,45 @@ if __name__ == "__main__":
     t1.join() 
     t2.join() 
 
+
+# ----------------------------CONCURRENT-FEATURES--------------------
+
+import concurrent.futures
+import time
+
+def do_something(seconds):
+    print(f"Sleeping {seconds} seconds ... ")
+    time.sleep(seconds)
+    return "done sleeping"
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    f1 = executor.submit(do_something, 1)
+    f2 = executor.submit(do_something, 1)
+    print(f1.result())
+    print(f2.result())
+import multiprocessing
+import concurrent.futures
+import time
+
+def do_something(seconds):
+    print(f"Sleeping {seconds} seconds ... ")
+    time.sleep(seconds)
+
+    return "done sleeping"
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    secs = [5,4,3,2,1]
+    results = executor.map(do_something, secs)
+
+    for result in results:
+        print(result)
+
+
+# with concurrent.futures.ProcessPoolExecutor() as executor:    
+#     f1 = executor.submit(do_something, 1)
+#     f2 = executor.submit(do_something, 1)
+
+
+#     print(f1.result())
+#     print(f2.result())
+
