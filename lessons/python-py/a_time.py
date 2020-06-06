0e4b2d5228b5660f279c002fd915c252cca1322d
mirlan <you@example.com>
2020-06-06 07:43:37 -0400

    add timedelta

--- lessons/python-py/a_time.py
+++ lessons/python-py/a_time.py
@@ -12,5 +12,25 @@ t = datetime.time(9,18,6)
 
 print(t.hour)
 
+==========================================================
+from datetime import datetime, date, timedelta
+import random
+
+for i in range(0,7):
+    print (date.today - timedelta(days=i))
+=============================================================
+
+
+start_time = datetime(year=2019, month=4, day=1).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+print(d2) 
+
+today = date(2014, 10, 3) # or you can do today = date.today() for today's date
+
+
+
+    @classmethod
+    def fromBirthYear(cls, name, birthYear):
+        return cls(name, date.today().year - birthYear)  # NOTE: proccess value '1985' and get '34' then return it as cls arg
 
 
