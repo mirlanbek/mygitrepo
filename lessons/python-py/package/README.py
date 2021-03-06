0e5bfcbd36e2b93305ae388e89d6daf982b39b34
Mickey <mirlanbek@users.noreply.github.com>
2021-03-05 23:16:30 -0800

    Update README.py
    update package and subpackage info

--- lessons/python-py/package/README.py
+++ lessons/python-py/package/README.py
@@ -103,5 +103,27 @@ package/sub_package/module.py   --------------->:   class ClassName(): var="sala
 
 """
 
+doc6 = """
+package/
+    __init__.py
+    subpackage1/
+        __init__.py
+        moduleX.py
+        moduleY.py
+    subpackage2/
+        __init__.py
+        moduleZ.py
+    moduleA.py
+    
+    -------------------------------------- ------------------------- 
+    
+from .moduleY import spam
+from .moduleY import spam as ham
+from . import moduleY
+from ..subpackage1 import moduleY
+from ..subpackage2.moduleZ import eggs
+from ..moduleA import foo
+    
 
+"""
 
