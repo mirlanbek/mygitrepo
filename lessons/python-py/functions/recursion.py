de2187000b22ea422a14a68ead36cdcf97844925
Mickey <mirlanbek@users.noreply.github.com>
2023-10-22 11:44:13 -0700

    Update recursion.py
    made some changes

--- lessons/python-py/functions/recursion.py
+++ lessons/python-py/functions/recursion.py
@@ -2,40 +2,40 @@ import os,sys
 
 # recursion  is kinda while loop or for loop, poka num ==0, num -1 
 
-def num_test (num):
-    if num == 0:
-        print ("end of nums")
+# =====   int Nums ======================================= 
+n = 7
+def recur(n):
+    if n > 0:
+        print(n)
+        recur(n-1)
     else:
-        print(num)
-        num_test (num - 1)  
-     
-num_test(3)
+        print("end of num")
 
+recur(n)
+
+# ============ List =======================================
+l = ["bir", "eki", "uch", "tort"]
+
+def recur(l):
+    if len(l) > 0:
+        print(l[0])
+        recur(l[1:])
+    else:
+        print("end of list")
+
+recur(l)
+
+================ real time recursion ==========================================
 
 list_1 = ['ES31-CTS', 'functional', 'synchronization', 'inter_call',
  'without_memory_barrier', 'ssbo_atomic_counter_mixed_dispatch_100_calls_1k_invocations']
 
-# list_1="mirlan"
-# print(list_1) 
 
-def recursive_repw(list_1):
-        
-        if len(list_1) <= 1:
-                return " 0 eken"
-                  
-        recursive_repw(list_1[1:])
-        print(  list_1[0]    )
+def recur(list_1):
+    if len(list_1) > 0:
+        print(list_1[0])
+        recur(list_1[1:])
+    else:
+        print("end of list")
 
-recursive_repw(list_1)                
-
-# ===================================================
-l = ["bir", "eki", "uch", "tort"]
-
-def recur(l):
-    if len(l) == 1:
-        return("it is done")
-    recur(l[1:])
-    print(l[0])
-     
-recur(l)
-
+recur(list_1)
